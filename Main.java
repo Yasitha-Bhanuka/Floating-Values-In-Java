7bb2003cf27930ead7e24bc5240647d38cbcbf19
Yasitha Bhanuka <127284944+Yasitha-Bhanuka@users.noreply.github.com>
2023-07-15 15:00:54 +0530

    Add the floating initialization part.
    Using specific way to initialize the floating-type variable in JAVA.

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // simply initialize the double-type variable.
         double number1 = 1.12325;
         System.out.println(number1); // output is 1.12325
+
+        /*float number2 = 1.12325;
+               Using this normal way we can see the error.*/
+
+        // specific way to initialize the float-type variable.
+        float number2 = 1.12325F; // should include the 'F' or 'f' before semicolon
+        System.out.println(number2); // output is 1.12325
+
     }
 }
